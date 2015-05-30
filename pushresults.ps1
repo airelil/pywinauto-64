3f056389764617b78a86ca8151fe565e84c0df3f
Valentin Kroupkin <assotut@gmail.com>
2015-05-30 16:26:50 +0300

    Call test results uploading directly from appveyor.yml

--- pushresults.ps1
+++
@@ -1,3 +0,0 @@
-# upload test results to AppVeyor
-$wc = New-Object 'System.Net.WebClient'
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\nosetests.xml))
